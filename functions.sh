bc1a1aa8bf290342b8bc6d06a97f5cc346f6e6a8
murthy <n4.murthy@gmail.com>
2026-03-23 18:30:13 +0530

    commit

---
+++ functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# FUNCTIONS IN SHELL SCRIPTING
+if [ $userid -ne 0 ]; then
+    echo "Please run with ROOT access"
+    exit 1
+fi
+validate()
+{
+    if [ $? -ne 0 ]; then
+        echo -e "Installing $2 .. FAILURE"
+    else    
+        echo -e "Installing $2 .. SUCCESS"
+}
+dnf install -y nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    validate $? "nginx"
+else
+    echo -e "ALready installed"
+fi
