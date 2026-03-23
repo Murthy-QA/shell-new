ded0a11bd90266e3483e391e6f6effc9f3e8f684
murthy <n4.murthy@gmail.com>
2026-03-23 16:58:58 +0530

    commit

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name="murthy"
+age=34
+place="hyderabad"
+job="SRE"
+echo "Name of person is:$name"
+echo "age of person is:$age"
+echo "place of person is:$place"
+echo "job of person is:$job"
