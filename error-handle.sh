3d10f5117c4956787eef054feaf57dd768c676da
murthy <n4.murthy@gmail.com>
2026-03-23 23:10:10 +0530

    commit

--- error-handle.sh
+++ error-handle.sh
@@ -3,4 +3,3 @@ trap 'echo "Error is in $LINENO"; exit 1' ERR
 
 cp file.txt file2.txt
 rm new.txt
-
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello, this is my cron job" >> /tmp/cron_test.log
+date >> /tmp/cron_test.log
+echo "----------------------" >> /tmp/cron_test.log
